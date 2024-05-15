6b24c9c1f176de85429dc3e11f5393c95cb7ff33
sandwipshanto <moloyswe20@gmail.com>
2024-05-15 23:45:33 +0600

    first commit

---
+++ DP_Composit/Client.java
@@ -0,0 +1,18 @@
+public class Client {
+    public static void main(String[] args) {
+        File file0 = new File(18);
+        File file1 = new File(76);
+        File file2 = new File(78);
+
+        Elements sub = new Elements();
+        Elements main = new Elements();
+
+        main.add(sub);
+        main.add(file0);
+        main.add(file1);
+        sub.add(file2);
+
+        System.out.println(main.getsize());
+
+    }
+}
